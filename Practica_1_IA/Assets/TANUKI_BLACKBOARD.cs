027d61d1c56219079f5ebf45f53d3f41d2630c61
Damian Lopez <demthewhite@gmail.com>
2020-02-29 01:38:07 +0100

    Creada FSM de los TANUKIS, falta Steering

---
+++ Practica_1_IA/Assets/TANUKI_BLACKBOARD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TANUKI_BLACKBOARD : MonoBehaviour
+{
+    public float foodDetectableRadius;
+    public float foodReachedRadius = 12;
+    public float timeToEat;
+
+   // public float orbitRadius;
+    public GameObject orbitTarget;
+    
+
+
+
+    void Awake()
+    {
+        orbitTarget = GameObject.Find("Attractor");
+
+    }
+}
