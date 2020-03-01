4b11414effe67248e9b0f22145c7305301a68adc
Damian Lopez <demthewhite@gmail.com>
2020-03-01 18:02:20 +0100

    Tanukis se tpean cuando cambian de orbit a arrive aun asi está hecho todo

---
+++ Practica_1_IA/Assets/Scripts/Steerings/Orbit_SB.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Steerings
+{
+    public class Orbit_SB : SteeringBehaviour
+    {
+        public RotationalPolicy rotationalPolicy = RotationalPolicy.FT;
+
+        private float angularSpeed;
+
+        public GameObject target;
+
+        public override SteeringOutput GetSteering()
+        {
+            if (this.ownKS == null) this.ownKS = GetComponent<KinematicState>();
+
+            SteeringOutput result = Orbit_SB.GetSteering(this.ownKS, target, angularSpeed);
+
+            base.applyRotationalPolicy(rotationalPolicy, result, target);
+            return result;
+        }
+
+        public static SteeringOutput GetSteering(KinematicState ownKms, GameObject target, float angularVelocity)
+        {
+            SteeringOutput steering = new SteeringOutput();
+
+            Vector3 desiredPosition;
+            Vector3 desiredVlocity;
+
+            //Moves object around the target
+            ownKms.transform.RotateAround(target.transform.position, target.transform.forward, angularVelocity * Time.deltaTime);
+            
+            //Calculates direction 
+            desiredPosition = ownKms.transform.position;
+            desiredVlocity = desiredPosition / Time.deltaTime;
+
+            //Applies direction update to the orbitating object
+            steering.angularAcceleration = Utils.VectorToOrientation(desiredVlocity / Time.deltaTime);
+
+            steering.angularActive = true;
+            steering.linearActive = false;
+
+            return steering;
+        }
+
+
+
+        public void SetAngularSpeed(float newSpeed)
+        {
+            angularSpeed = newSpeed;
+        }
+
+    }
+}
---
+++ Practica_1_IA/Assets/Scripts/TANUKI_BLACKBOARD.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TANUKI_BLACKBOARD : MonoBehaviour
+{
+    
+    public float SharpieDetectableRadius = 500;
+    
+    public float orbitRadius;
+    public float tooFarFromSharpie = 130f;
+
+   // public float orbitRadius;
+    public GameObject orbitTarget;
+    
+
+
+
+    void Awake()
+    {
+        orbitTarget = GameObject.Find("SHARPIE");
+        orbitRadius = 100f;
+
+    }
+}
